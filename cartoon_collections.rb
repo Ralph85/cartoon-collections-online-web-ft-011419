9f68c1313f8b30dbbe06d2b84d653642938623f3
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-18 05:38:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
+  puts Doc
+  puts Dopey
+  puts Bashful
+  puts Grumpy
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  ["EARTH!", "WIND!", "FIRE!", "WATER!", "HEART!"]
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
